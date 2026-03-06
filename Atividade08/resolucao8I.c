5a336ae31489ac02037f03c075f1d8aff73cbe4f
OgrodoCerrado <igorpechir@yahoo.com.br>
2026-03-06 17:22:01 -0300

    Resolucao8 incompleta!

---
+++ Atividade08/resolucao8I.c
@@ -0,0 +1,4 @@
+//Resposta letra A) A função free libera a memória que estava alocada naquele ponteiro
+//Resposta letra B) Não, ele se torna inválido
+//Resposta letra c) Deixar de liberar memória 
+//Resposta letra D)
