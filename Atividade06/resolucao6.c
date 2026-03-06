29526f599ad3783d0c34e88b4b44fb3035afce3d
OgrodoCerrado <igorpechir@yahoo.com.br>
2026-03-06 16:17:18 -0300

    Resolucao6

---
+++ Atividade06/resolucao6.c
@@ -0,0 +1,5 @@
+//Resposta letra A) while:0 while:1 while:2 while:3 while:4 while:5 while:6 while:7 while:8 while:9 do:10
+//Resposta letra B) A ordem com que as funções são executadas, no while, ele verifica antes de executar e no do while, ele executa antes de verificar
+//Resposta letra C) Não entendi 
+//Resposta letra D) A função for delimita inicio, fim e processo, sendo melhor para situações quando voce sabe exatamente quantas vezes o laço vai se repetir
+//a função while verifica se uma afirmação é verdadeira e depois executa um processo, até que a afirmação se torne falsa, melhor utilizada em situações onde não sabe quantas repetições serão necessárias
