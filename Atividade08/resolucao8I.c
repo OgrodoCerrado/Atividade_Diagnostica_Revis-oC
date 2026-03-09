f7998ce25334ce7f0ce5d1507efa9a1c7757b6e9
OgrodoCerrado <igorpechir@yahoo.com.br>
2026-03-09 17:21:59 -0300

    exercicio 8 completo e 11

--- Atividade08/resolucao8I.c
+++ Atividade08/resolucao8I.c
@@ -1,4 +1,6 @@
 //Resposta letra A) A função free libera a memória que estava alocada naquele ponteiro
 //Resposta letra B) Não, ele se torna inválido
 //Resposta letra c) Deixar de liberar memória 
-//Resposta letra D)
+//Resposta letra D) Ela retorna um ponteiro nulo
+//Resposta letra E) O calloc reserva memória, mas também limpa o espaço antes de entregar ao usuário
+//Resposta letra F) O malloc reserva de forma rapida a memória, porém com lixo, já o calloc incializa todos os bits da memória alocada com 0
---
+++ Atividade11/Resolucao11.c
@@ -0,0 +1,1 @@
+//Não soube responder de cabeça, não me recordo de ter estudado essa matéria
